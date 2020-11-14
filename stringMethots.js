1bb668128770dc91fbe6ef8e8c5d5d7b481ef907
krisamorim <kris.a.furtado@gmail.com>
2020-11-14 13:31:12 -0300

    practice string methods

--- stringMethots.js
+++ stringMethots.js
@@ -1,7 +1,7 @@
 //lenth -> show the number of characters
 let anString = "This is a text";
 console.log(anString.length)
-//or
+//or directly on console 
 console.log("this is another text".length)
 
 //indexOf -> search some info inside the strings
@@ -9,3 +9,29 @@ console.log(anString.indexOf("text"))
 
 //slice ->
 console.log(anString.slice(0,4))
+
+//get word night inside a string
+let phrase = "Good night";
+
+function fastReplacement(text, wordSearched, wordReplaced){
+  return phrase.replace(wordSearched, wordReplaced)
+}
+
+console.log(fastReplacement(phrase, "night", "afternoon"))
+
+//get name kris from an object - just to practice
+//creating an object
+let me = {
+  name: "Kris",
+  age: 35,
+  city: "london"
+}
+
+//converting to string
+me = JSON.stringify(me);
+
+//get the kris position
+let name = me.indexOf("Kris");
+
+//cut the string from position 9 to 13
+console.log(me.slice(name,13))
