3380e8400e3612720c3dba47c811a7d55367699a
krisamorim <kris.a.furtado@gmail.com>
2022-02-11 19:27:42 -0300

    CSharp program version 2

--- CSharp/program.cs
+++ CSharp/program.cs
@@ -1,5 +1,5 @@
-//Version 1
-//get data -> show access permissions
+//Version 2
+//get data -> two options to choose
 
 using System;
 using System.Collections.Generic;
@@ -15,17 +15,17 @@ namespace identificarIdade
         {
             if (xx > 18)
             {
-                Console.WriteLine(yy+" você está autorizado");
+                Console.WriteLine(yy + " você está autorizado");
                 Console.ReadLine();
             }
             else if (xx > 11 && xx < 18)
             {
-                Console.WriteLine(yy+" vôcÊ é muito jovem");
+                Console.WriteLine(yy + " você é muito jovem");
                 Console.ReadLine();
             }
             else
             {
-                Console.WriteLine(yy+" você é criança ainda");
+                Console.WriteLine(yy + " você é criança ainda");
                 Console.ReadLine();
             }
         }
@@ -34,11 +34,36 @@ namespace identificarIdade
             Console.WriteLine("Qual seu nome?");
             var name = Console.ReadLine();
 
-            Console.WriteLine("Qual sua idade:");
-            var idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Olá " + name + "! O que deseja fazer? 1- Verificar permissão ou 2- Criar lista");
+            var opcao = int.Parse(Console.ReadLine());
 
-            calcularIdade(idade, name);
-            
+            if (opcao == 1)
+            {
+                Console.WriteLine("Qual sua idade:");
+                int idade = int.Parse(Console.ReadLine());
+                calcularIdade(idade, name);
+            }else if (opcao == 2)
+            {
+                Console.WriteLine("Digite o primeiro produto:");
+                var prod1 = Console.ReadLine();
+
+                Console.WriteLine("Digite o segundo produto:");
+                var prod2 = Console.ReadLine();
+
+                Console.WriteLine("Digite o terceiro produto:");
+                var prod3 = Console.ReadLine();
+
+                string[] produtos = new string[3]
+                {
+                    prod1,
+                    prod2,
+                    prod3
+                };
+
+                Console.WriteLine("A lista é: " + produtos);
+                Console.ReadLine();
+            }
         }
     }
 }
+
