a61e9a55b25b5abfb5c4c01397706a1479d51996
krisamorim <kris.a.furtado@Gmail.com>
2022-03-15 15:25:56 -0300

    Ex016 e Ex017

---
+++ Python/cursoemvideo/Ex016 - QuebrandoUmNumero.py
@@ -0,0 +1,6 @@
+from math import trunc
+numReal = float(input('Digite um número real: '))
+print('O valor digitado foi {}, contudo, sua parte inteira é {}'.format(numReal, trunc(numReal)))
+
+#outra forma
+print(int(numReal))
---
+++ Python/cursoemvideo/Ex017 - Catetos e Hipotenusa.py
@@ -0,0 +1,9 @@
+#forma 2 (com importação)
+
+co = float(int('Comprimento do cateto oposto: '))
+ca = float(int('Comprimento do cateto adjacente: '))
+
+#forma 1 (sem importação)
+hi = (co ** 2 + ca ** 2) ** (1/2)
+
+print('A hipotenusa vai medir {:.2f}'.format(h1))
