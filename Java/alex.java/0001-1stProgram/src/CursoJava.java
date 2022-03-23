4888c7e455cc0e5a643346eda865ead6addbd185
krisamorim <kris.a.furtado@Gmail.com>
2022-03-23 11:37:07 -0300

    Java e comandos git

---
+++ Java/alex.java/0001-1stProgram/src/CursoJava.java
@@ -0,0 +1,5 @@
+class CursoJava {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
---
+++ Java/alex.java/0002-1stProgramWithEclipse/src/cursojava/executavel/PrintConsole.java
@@ -0,0 +1,9 @@
+package cursojava.executavel;
+
+public class PrintConsole {
+	/*Main é um método autoexecutavel no Java*/
+	public static void main(String[] args) {
+		/*Escreve texto no console */
+		System.out.println("Hello WOrld");
+	}
+}
