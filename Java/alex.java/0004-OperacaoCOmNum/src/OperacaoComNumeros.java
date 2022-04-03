1bc71215376481232366ff98e7b8d4608ca245bd
krisamorim <kris.a.furtado@Gmail.com>
2022-04-02 23:09:38 -0300

    Trabalhadno com int e double

---
+++ Java/alex.java/0004-OperacaoCOmNum/src/OperacaoComNumeros.java
@@ -0,0 +1,21 @@
+
+public class OperacaoComNumeros {
+
+	public static void main(String[] args) {
+		/*inteiros*/
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		/*double*/
+		double dnota1 = 90;
+		double dnota2 = 70;
+		double dnota3 = 50;
+		double dnota4 = 88;
+
+		System.out.println("Média das notas = " + (nota1+nota2+nota3+nota4)/4);
+		System.out.println("Média das notas com variavel double = " + (dnota1+dnota2+dnota3+dnota4)/4);
+	}
+
+}
