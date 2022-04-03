a4c18b8b41091b80ce66a4e40291755b2d146e17
krisamorim <kris.a.furtado@Gmail.com>
2022-04-03 13:31:44 -0300

    char e condicional

---
+++ Java/alex.java/0005-caracter/src/Caracter.java
@@ -0,0 +1,19 @@
+
+public class Caracter {
+
+	public static void main(String[] args) {
+
+		/*Char Tratar letra*/
+		char pessoaFeminino = 'F';
+		
+		if(pessoaFeminino == 'F') {
+			System.out.println("Feminino");
+		}else{
+			System.out.println("Masculino");
+		}
+		
+		String textoQualquer = "qualquerCoisa";
+		
+	}
+
+}
---
+++ Java/alex.java/0006-IfElse/src/CondicionalIfElse.java
@@ -0,0 +1,22 @@
+
+public class CondicionalIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double nota1 = 7;
+		double nota2 = 7;
+		double nota3 = 7;
+		double nota4 = 6;
+		double media = (nota1+nota2+nota3+nota4)/4;
+		 /*se  preprecisar fazer recuperacao essa variavel será utilizada*/
+		double notaRcuperação = 
+		
+		if (media > 7) {
+			System.out.println("Aprovado direto");
+		}else{
+			System.out.println("Reprovado");
+		}
+	}
+
+}
