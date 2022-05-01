bf266cc175b38c6bab8a8a3527884cae51b3c5c3
Krishnamurtir <kris.a.furtado@gmail.com>
2022-05-01 14:27:11 -0300

    COrrigindo arquivo sobre corres no terminal

--- Coder/Python/cursoemvideo/Mundo01/000Bonus - UsandoCoresNoTerminal.py
+++ Coder/Python/cursoemvideo/Mundo01/000Bonus - UsandoCoresNoTerminal.py
@@ -1,9 +1,9 @@
 print('\033[1;31;43mVermelho no amarelo NEGRITO\033[m')
-print('\033[4;40;45mBranco no roxo SUBLINHADO\033[m')
+print('\033[4;0;45mBranco no roxo SUBLINHADO\033[m')
 print('\033[0;30;41mBranco no vermelho\033[m')
 print('\033[4;33;44mAmarelo no azul SUBLINHADO\033[m')
 print('\033[1;35;43mRoxo no amarelo NEGRITO\033[m')
-print('\033[33;42mBranco no Verde\033[m')
+print('\033[;42mBranco no Verde\033[m')
 print('\033[mCinza no preto\033[m')
 print('\033[7;30mPreto no cinza\033[m')
 
@@ -14,6 +14,6 @@ print('\033[7;30mPreto no cinza\033[m')
 #4 underline
 #7 negativo
 
-#cor: 30-branco 31-Vermelho 32-Verde 33-Amarelo 34-Azul 35-Roxo 36-AzulClaro 37 Cinza
+#cor: 30-Preto 31-Vermelho 32-Verde 33-Amarelo 34-Azul 35-Roxo 36-AzulClaro 37 Cinza
 
 #background de 40 a 47
