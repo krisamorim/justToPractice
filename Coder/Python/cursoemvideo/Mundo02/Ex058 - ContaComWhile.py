f7d903672d97d27de0440b2d075aff03e26125d4
Krishnamurtir <kris.a.furtado@gmail.com>
2022-05-22 01:27:23 -0300

    Comandos docker

---
+++ Coder/Python/cursoemvideo/Mundo02/Ex058 - ContaComWhile.py
@@ -0,0 +1,14 @@
+from random import randint
+numSorteado = randint(0,10)
+numEscolhido = int(input('Digite o número de 1 a 10 que você acha que foi sorteado: '))
+tentativas = 0
+while numEscolhido != numSorteado:
+    numEscolhido = int(input('\033[1:31mErrado!!\033[m Tente novamente\nDigite o número de 1 a 10 que você acha que foi sorteado: '))
+    tentativas += 1
+
+if tentativas < 2:
+    print('\033[:42mParabéns!!!\033[m Você acertou de primeira.\nO número sorteado foi {}'.format(numSorteado))
+elif tentativas < 3:
+    print('\033[32:41mParabéns!!!\033[m Você acertou com menos de 3 tentativas\nO número sorteado foi {}'.format(numSorteado))
+else:
+    print('Nossa!!! Finalmente em!! \033[32:41mParabéns!!!\033[m você acertou.\nO número sorteado foi {}'.format(numSorteado))
