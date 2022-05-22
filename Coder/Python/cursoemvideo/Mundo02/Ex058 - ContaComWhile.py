b531aab55895310a90ac0fbdbf549d19e3332c2c
Krishnamurtir <kris.a.furtado@gmail.com>
2022-05-22 01:57:33 -0300

    Exercicio 058

--- Coder/Python/cursoemvideo/Mundo02/Ex058 - ContaComWhile.py
+++ Coder/Python/cursoemvideo/Mundo02/Ex058 - ContaComWhile.py
@@ -1,7 +1,7 @@
 from random import randint
 numSorteado = randint(0,10)
 numEscolhido = int(input('Digite o número de 1 a 10 que você acha que foi sorteado: '))
-tentativas = 0
+tentativas = 1
 while numEscolhido != numSorteado:
     numEscolhido = int(input('\033[1:31mErrado!!\033[m Tente novamente\nDigite o número de 1 a 10 que você acha que foi sorteado: '))
     tentativas += 1
@@ -11,4 +11,4 @@ if tentativas < 2:
 elif tentativas < 3:
     print('\033[32:41mParabéns!!!\033[m Você acertou com menos de 3 tentativas\nO número sorteado foi {}'.format(numSorteado))
 else:
-    print('Nossa!!! Finalmente em!! \033[32:41mParabéns!!!\033[m você acertou.\nO número sorteado foi {}'.format(numSorteado))
+    print('Nossa!!! Finalmente em!! Você acertou depois de {} tentativas \033[32:41mParabéns!!!\033[m você acertou.\nO número sorteado foi {}'.format(tentativas, numSorteado))
