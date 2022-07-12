3872b7030ce433eab38ca89d2726cffcea4ab8e9
krisamorim <kris.a.furtado@Gmail.com>
2022-07-11 22:12:17 -0300

    Aula002 - criando objetos atualização

--- Coder/Kotlin/Aula0002/src/main/kotlin/Main.kt
+++ Coder/Kotlin/Aula0002/src/main/kotlin/Main.kt
@@ -16,6 +16,24 @@ fun main() {
 
     //utilizando get
     println(userA.getNomeEmCaixaBaixa())
+
+    //utilizando propriedade de atualizar nome
+    userA.atualizarNome("Pedro")
+
+    //mostrando como criar um objeto com mais de uma propriedade
+    val botaoSucesso = Botao()
+    botaoSucesso.texto = "Sucesso"
+    botaoSucesso.cor = "Green"
+
+    val botaoCancelar = Botao()
+    botaoCancelar.texto = "Cancelar"
+    botaoCancelar.cor = "Red"
+
+    //objeto com mais de uma propriedade
+    val filmeVingadores1 = Filme()
+    filmeVingadores1.titulo = "Os Vingadores"
+    filmeVingadores1.descricao = "O Filme conta a origem dos vingadores"
+    filmeVingadores1.elenco = "Scarlett Johansson, Chris Evans, Mark Ruffalo, Robert Downey, CHris Hemsworth, Samuel L Jackson, Jeremy Renner"
 }
 
 class Usuario {
@@ -30,8 +48,21 @@ class Usuario {
     fun getNomeEmCaixaBaixa(): String{
         return nome.lowercase()
     }
+
+    //atualiza o valor da variavel nome e mostra mensagem no console
+    fun atualizarNome (novoNome: String){
+        var nomeAntigo = nome
+        nome = novoNome
+        println("Nome atualizado de $nomeAntigo para $nome")
+    }
 }
 
+class Botao {
+    var texto: String = ""
+    var cor: String = ""
+}
+
+//criando um objeto com mais de uma propriedade
 class Filme {
     var titulo: String = ""
     var descricao: String = ""
