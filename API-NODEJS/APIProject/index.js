4790aa7f7d200d264dad3fd60a333fd5a6627bcd
krisamorim <kris.a.furtado@Gmail.com>
2022-10-11 15:29:21 -0300

    variávei qeu recebem as bibliotecas

---
+++ API-NODEJS/APIProject/index.js
@@ -0,0 +1,5 @@
+// 1- Criando as variáveis para armazenar as bibliotecas
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
