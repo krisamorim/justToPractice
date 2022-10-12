56937838138c0fbf14b0b04e148320257fb46303
krisamorim <kris.a.furtado@Gmail.com>
2022-10-11 23:18:15 -0300

    config json para funcionar com nodemon

--- API-NODEJS/APIProject/index.js
+++ API-NODEJS/APIProject/index.js
@@ -12,3 +12,7 @@ app.use(morgan('dev'))//morgan ao projeto para que tenhamos o log de execução
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.json()) //serve para informar o tipo de dados que queremos receber, que no nosso caso será um JSON
 app.use(cors())// para definir quem pode comunicar coma  api
+
+app.listen(8081, () => {
+    console.log('Express iniciado em http://localhost:8081')
+})
