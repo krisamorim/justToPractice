eeafdd65910ae038d3b0f73fb95da4d17aa307fb
krisamorim <kris.a.furtado@Gmail.com>
2023-03-17 20:07:15 -0300

    0001-Criado a Classe aluno e seus atributos. Essa classe irá virá um objeto posteriormente

---
+++ Coder/Java/alex.java/0022-primeiro-programa-java/src/programa/classes/Aluno.java
@@ -0,0 +1,16 @@
+package programa.classes;
+
+/*Clase/Objeto que representa aluno*/
+public class Aluno {
+	//Atributos:
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomePai;
+	String nomeMae;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+}
