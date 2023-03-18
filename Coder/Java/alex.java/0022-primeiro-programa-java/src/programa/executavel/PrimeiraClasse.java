5cc9841da652713e4c8fb4543810233ceaaa0632
krisamorim <kris.a.furtado@Gmail.com>
2023-03-18 05:45:32 -0300

    0003-Instanciando objeto na memória

--- Coder/Java/alex.java/0022-primeiro-programa-java/src/programa/executavel/PrimeiraClasse.java
+++ Coder/Java/alex.java/0022-primeiro-programa-java/src/programa/executavel/PrimeiraClasse.java
@@ -6,10 +6,10 @@ public class PrimeiraClasse {
 	
 	public static void main(String[] args) {
 		
-		/* Se o objeto estiver declarado como mostrado abaixo ele não existe na memória*/
-		Aluno aluno1;
+
+		Aluno aluno1 = new Aluno();/*Aqui será o João*/
 		
 		/*Ja se o objeto for declarado com o new, ele parra a existir na memória*/
-		Aluno aluno2 = new Aluno();
+		Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
 	}
 }
