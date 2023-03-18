4eecca4c64f4f08bda8cd6325b1e851b0efcb917
krisamorim <kris.a.furtado@Gmail.com>
2023-03-18 05:50:33 -0300

    0005-Utilizando o contrutor nomePadrao

--- Coder/Java/alex.java/0022-primeiro-programa-java/src/programa/executavel/PrimeiraClasse.java
+++ Coder/Java/alex.java/0022-primeiro-programa-java/src/programa/executavel/PrimeiraClasse.java
@@ -11,5 +11,7 @@ public class PrimeiraClasse {
 		
 		/*Ja se o objeto for declarado com o new, ele parra a existir na memória*/
 		Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
+		
+		Aluno aluno3 = new Aluno("Maria");//utilizando o contrutor nomePadrao
 	}
 }
