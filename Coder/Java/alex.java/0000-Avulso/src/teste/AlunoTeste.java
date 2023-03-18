b958b71b83399b7e532bbde7a09d799f59b8a0b4
krisamorim <kris.a.furtado@Gmail.com>
2023-03-18 06:40:03 -0300

    Entendeo melhor sobre instâncias: Criei uma classe no mesmo pacote e notei que o atributo é acessivel mesmo sem ser definido como public

---
+++ Coder/Java/alex.java/0000-Avulso/src/teste/AlunoTeste.java
@@ -0,0 +1,15 @@
+package teste;
+
+public class AlunoTeste {
+	String nome;
+	int idade;
+	
+	public AlunoTeste() {
+		
+	}
+
+	public AlunoTeste(String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
--- Coder/Java/alex.java/0000-Avulso/src/teste/ClassToTest.java
+++ Coder/Java/alex.java/0000-Avulso/src/teste/ClassToTest.java
@@ -2,6 +2,12 @@ package teste;
 
 public class ClassToTest {
 	public static void main(String[] args) {
-		System.out.println("Use this class to do tests");
+		
+		AlunoTeste aluno1 = new AlunoTeste("Kris", 38);
+		AlunoTeste aluno2 = new AlunoTeste();
+		aluno2.nome = "Ana";
+		
+		System.out.println(aluno1.nome);
+		System.out.println(aluno2.nome);
 	}
 }
